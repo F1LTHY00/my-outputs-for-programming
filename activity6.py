107a9ea4b5aba35f12bbcce5c8df59e8cb56fb7e
F1LTHY00 <zacdeeznutseseo@gmail.com>
2025-08-13 15:03:21 +0800

    Add files via upload

---
+++ activity6.py
@@ -0,0 +1,13 @@
+n1 = eval(input("Enter the first number : "))
+n2 = eval(input("Enter the second number : "))
+s = n1 + n2
+a = n1 - n2
+m = n1 * n2
+b = n1 / n2
+print("\nThe sum of",n1,"and",n2,"is",s)
+print("The difference of",n1,"and",n2,"is",a)
+print("The product of",n1,"and",n2,"is",m)
+print("The qoutient of",n1,"and",n2,"is",b)
+print(n1, "exponent by",n2,"is",n1**n2)
+print("The remainder of",n1,"and",n2,"is",n1 % n2)
+print("The floor division of",n1,"and",n2,"is",n1//n2)
