ed763d531b0d99dfa49b7eb986bf179441c5bb3b
F1LTHY00 <zacdeeznutseseo@gmail.com>
2025-08-13 15:24:07 +0800

    code_challenge1.py

--- code_challenge1.py
+++ code_challenge1.py
@@ -1,3 +1,4 @@
 name = input ("what is your name ?")
 
-print ("\t\t\t\t*\t\t\n\n\t\t\t*\t\t*\n\n\t\t*\t\t\t\t*\t\n\n\t*\t\t\t\t\t\t*\n\n\n*\t\t\thi \t\t\t\t\t*\n\n\n\t*\t\t jp \t\t\t\t*\n\n\t\t*\t\t\t\t*\n\n\n\t\t\t*\t\t*\n\n\n\t\t\t\t*") 
+
+print ("\t\t\t\t*\t\t\n\n\t\t\t*\t\t*\n\n\t\t*\t\t\t\t*\t\n\n\t*\t\t\t\t\t\t*\n\n\n*\t\t\thi \t\t\t\t\t*\n\n\n\t*\t\t",name,"\t\t\t\t*\n\n\t\t*\t\t\t\t*\n\n\n\t\t\t*\t\t*\n\n\n\t\t\t\t*") 
