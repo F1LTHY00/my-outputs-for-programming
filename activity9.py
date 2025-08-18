81264d3d87e0a8356581559bc4db33022ae167bd
F1LTHY00 <zacdeeznutseseo@gmail.com>
2025-08-18 15:49:24 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,14 @@
+a = 10
+b = 13
+
+c = 24
+d = 22
+
+username = 'sam'
+password = 'bading'
+
+print (username == 'sam' and (password == 'bading' )) 
+print (username == 'sam' or (password == 'bading' ))  
+print ( (a > b) and (c > d) )
+print ( (a < b) or (c > d) )
+print (not((a >b) or (c < d)))
