ff198f1e448f96ce6264ce8e26d3861087baa3b2
F1LTHY00 <zacdeeznutseseo@gmail.com>
2025-08-27 15:59:57 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,19 @@
+name = input(" what is your name ? ")
+fare = input(" how much are you going to pay ? ")
+student = input(" are you a student ? ")
+discount = int(fare) * .20
+discount_fare = float(discount -
+
+if student == 'Yes':
+	print("The 20% discount is applied ")
+
+if student == 'yes':
+	print("The 20% discount is applied ")
+
+if student == 'No':
+	print("The 20% discount will not be applied ")
+
+if student == 'no':
+	print("The 20% discount will not be applied ")
+
+ 
