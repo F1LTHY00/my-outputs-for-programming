0d469735206953b2b5e76a32b8171c9e1bb60227
F1LTHY00 <zacdeeznutseseo@gmail.com>
2025-08-27 15:15:01 +0800

    Add files via upload

---
+++ Activity10.py
@@ -0,0 +1,12 @@
+print ("Enter your Username -->")
+Username = input()
+
+print ("Enter your Password -->")
+Password = input ()
+
+if (Username == 'F1LTHY00') and (Password == 'pbcalbum00'):
+		print ("Login Successful")
+
+
+if (Username != 'F1LTHY00') and (Password != 'pbcalbum00'):
+		print ("Login Failed")	
