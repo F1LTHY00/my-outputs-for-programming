f9815215a4b43c797eb66f0d7bc1a69ee47cf6c1
F1LTHY00 <zacdeeznutseseo@gmail.com>
2025-09-17 21:51:41 +0800

    Add files via upload

---
+++ activity12.py
@@ -0,0 +1,2 @@
+for SamBading in range (1, 101, 1):
+    print (SamBading, '. Sam bading')
---
+++ activity13.py
@@ -0,0 +1,7 @@
+a = 0
+
+for y in range (1, 11, 1):
+    x = eval(input("Enter a number: "))
+    a += x
+
+print ("The sum of all the numbers given is: ",a)
