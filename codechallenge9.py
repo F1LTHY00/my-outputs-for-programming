0e93c6898fb9a5c7cccb42e6add2e708406fd953
F1LTHY00 <zacdeeznutseseo@gmail.com>
2025-09-17 21:57:12 +0800

    codechallenge9.py

--- codechallenge9.py
+++ codechallenge9.py
@@ -1,6 +1,7 @@
 print ("squawk")
 
 sam = input("Tell something to the parrot 😀: ")
-bading = eval(input("How many time would you like the parrot to repeat it ? "))
+bading = eval(input("How many times would you like the parrot to repeat it ? "))
 for y in range(bading):
+
     print ("Squawk! 🦜", sam)
